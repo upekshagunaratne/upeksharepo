ab0734b7fc68f9fb29bb94662cf8f2c50002dc52
Gemunu <narangalad@gmail.com>
2025-01-18 19:58:58 +1100

    1.fixed img  path 2.Added click event

--- src/app/data/users/dummy-users.ts
+++ src/app/data/users/dummy-users.ts
@@ -2,31 +2,31 @@ export const DUMMY_USERS = [
   {
     id: 'u1',
     name: 'Jasmine Washington',
-    imagePath: 'C:/Users/Administrator/Test-004/src/app/user-1.jpg',
+    imagePath: 'users/user-1.jpg',
   },
   {
     id: 'u2',
     name: 'Emily Thompson',
-    imagePath: 'user-2.jpg',
+    imagePath: 'users/user-2.jpg',
   },
   {
     id: 'u3',
     name: 'Marcus Johnson',
-    imagePath: 'user-3.jpg',
+    imagePath: 'users/user-3.jpg',
   },
   {
     id: 'u4',
     name: 'David Miller',
-    imagePath: 'user-4.jpg',
+    imagePath: 'users/user-4.jpg',
   },
   {
     id: 'u5',
     name: 'Priya Patel',
-    imagePath: 'user-5.jpg',
+    imagePath: 'users/user-5.jpg',
   },
   {
     id: 'u6',
     name: 'Arjun Singh',
-    imagePath: 'user-6.jpg',
+    imagePath: 'users/user-6.jpg',
   },
 ];
--- src/app/left-navbar/left-navbar-button/left-navbar-button.component.ts
+++ src/app/left-navbar/left-navbar-button/left-navbar-button.component.ts
@@ -1,16 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../../data/users/dummy-users';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-left-navbar-button',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './left-navbar-button.component.html',
   styleUrl: './left-navbar-button.component.css'
 })
 export class LeftNavbarButtonComponent {
  users = DUMMY_USERS;
-  
+ 
+ selectedUserName: string | null = null;
+ 
+ displayUserName(name: string): void {
+  this.selectedUserName = name;
+ }
+
 }
